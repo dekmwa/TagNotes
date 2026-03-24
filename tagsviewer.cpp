124677d6a22de250e74f04e552cc1ffb2d54b181
Sergey <sserz.h@bk.ru>
2026-03-24 07:23:31 +0300

    Новые методы в TagsViewer

--- tagsviewer.cpp
+++ tagsviewer.cpp
@@ -1,5 +1,58 @@
 #include "tagsviewer.h"
 
-TagsViewer::TagsViewer(QWidget *parent)
-    : QWidget{parent}
-{}
+TagsViewer::TagsViewer(QWidget *parent) : QWidget{parent}, database(Database::instance()),
+    m_mainLay(new QFlowLayout())
+{
+
+}
+
+void TagsViewer::addTag(int tagId) {
+    if (m_tags.contains(tagId)) {
+        qDebug() << "TagsViewer::addTag - тег уже есть";
+        return;
+    }
+
+    QString title = database.getTagTitleById(tagId);
+    if (title.isEmpty()) {
+        qDebug() << "TagsViewer::addTag - тег не найден";
+        return;
+    }
+
+    QPushButton *tagBtn = new QPushButton();
+    tagBtn->setText(title);
+
+    m_tags.insert(tagId, tagBtn);
+    m_mainLay->addWidget(tagBtn);
+
+    connect(tagBtn, &QPushButton::clicked, [this, tagId](){
+        emit tagClicked(tagId);
+    });
+}
+
+void TagsViewer::removeTag(int tagId) {
+    if (!m_tags.contains(tagId)) {
+        qDebug() << "TagsViewer::addTag - тега нет";
+        return;
+    }
+
+    m_mainLay->removeWidget(m_tags[tagId]);
+    m_tags.remove(tagId);
+
+    emit tagRemoved(tagId);
+}
+
+void TagsViewer::clearAll() {
+    for (auto it = m_tags.begin(); it != m_tags.end(); ++it) {
+        m_mainLay->removeWidget(it.value());
+        delete it.value();
+    }
+    m_tags.clear();
+}
+
+QVector<int> TagsViewer::getTagIds() {
+    QVector<int> ids;
+    for (auto it = m_tags.begin(); it != m_tags.end(); ++it) {
+        ids.append(it.key());
+    }
+    return ids;
+}
--- tagsviewer.h
+++ tagsviewer.h
@@ -2,14 +2,32 @@
 #define TAGSVIEWER_H
 
 #include <QWidget>
+#include "qflowlayout.h"
+#include <QPushButton>
+#include <QMap>
+#include "database.h"
+
 
 class TagsViewer : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit TagsViewer(QWidget *parent = nullptr);
 
+    void addTag(int tagId);
+    void removeTag(int tagId);
+    void clearAll();
+    QVector<int> getTagIds();
+
+private:
+    Database& database;
+    QFlowLayout *m_mainLay;
+    QMap<int, QPushButton*> m_tags;
+
 signals:
+    void tagClicked(int tagId);
+    void tagRemoved(int tagId);
 };
 
 #endif // TAGSVIEWER_H
