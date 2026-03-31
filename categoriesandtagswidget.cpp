0e494757c769c59f4a486175147c95dfd4beab8d
Artem <sserz.h@bk.ru>
2026-03-31 19:36:21 +0300

    Новый виджет просмотра категорий и тегов

---
+++ categoriesandtagswidget.cpp
@@ -0,0 +1,64 @@
+#include "categoriesandtagswidget.h"
+
+CategoriesAndTagsWidget::CategoriesAndTagsWidget(QWidget *parent, Mode mode) : QWidget{parent},
+    database(Database::instance()),
+    currentMode(mode),
+    mainLay(new QVBoxLayout(this)),
+    tagsViewer(new TagsViewer(this, WidgetMode::WITH_PLUS_BUTTON)),
+    categoriesViewer(new CategoriesViewer(this))//,
+    //addTagButton(new QPushButton(this))
+{
+    connect(categoriesViewer, &CategoriesViewer::categoryClicked, [this](int categoryId){
+        updateTagsByCategoryId(categoryId);
+    });
+
+    setupWidget();
+    refresh();
+}
+
+void CategoriesAndTagsWidget::setupWidget() {
+    QHBoxLayout *categoriesAndPlus = new QHBoxLayout();
+    QPushButton *addCategory = new QPushButton(this);
+    addCategory->setText("+");
+    addCategory->setFixedWidth(20);
+    if (currentMode == Mode::VIEW_ONLY) addCategory->hide();
+
+    categoriesAndPlus->addWidget(categoriesViewer);
+    categoriesAndPlus->addWidget(addCategory);
+
+    mainLay->addLayout(categoriesAndPlus);
+
+    QHBoxLayout *tagsAndPlus = new QHBoxLayout();
+    //if (currentMode == Mode::VIEW_ONLY) addTagButton->hide();
+
+    tagsAndPlus->addWidget(tagsViewer);
+    //tagsAndPlus->addWidget(addTagButton);
+
+    mainLay->addLayout(tagsAndPlus);
+    setLayout(mainLay);
+}
+
+void CategoriesAndTagsWidget::refresh() {
+    categoriesViewer->updateCategories();
+}
+
+void CategoriesAndTagsWidget::updateTagsByCategoryId(int categoryId) {
+    tagsViewer->clearAll();
+    currentSelectedCategoryId = categoryId;
+    //addTagButton->setDisabled(false);
+
+    QMap<int, QString> tags = database.getTagsByCategoryId(categoryId);
+
+    for (const auto& tagId : tags.keys()) {
+        tagsViewer->addTag(tagId);
+        connect(tagsViewer, &TagsViewer::tagClicked, this, [this, tagId](){
+            emit onTagClicked(tagId);
+        });
+    }
+
+    tagsViewer->unlockAddButton();
+}
+
+void CategoriesAndTagsWidget::updateTagsByCurrentCategory() {
+    updateTagsByCategoryId(currentSelectedCategoryId);
+}
---
+++ categoriesandtagswidget.h
@@ -0,0 +1,41 @@
+#ifndef CATEGORIESANDTAGSWIDGET_H
+#define CATEGORIESANDTAGSWIDGET_H
+
+enum class Mode {
+    VIEW_ONLY,
+    ADD_TAGS_AND_ADD_CATEGORIES
+};
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include "tagsviewer.h"
+#include "categoriesviewer.h"
+#include "database.h"
+
+
+class CategoriesAndTagsWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CategoriesAndTagsWidget(QWidget *parent, Mode mode);
+
+private:
+    Database& database;
+    Mode currentMode;
+    QVBoxLayout *mainLay;
+    TagsViewer *tagsViewer;
+    CategoriesViewer *categoriesViewer;
+    int currentSelectedCategoryId;
+    //QPushButton *addTagButton;
+
+    void setupWidget();
+    void refresh();
+    void updateTagsByCategoryId(int categoryId);
+    void updateTagsByCurrentCategory();
+
+signals:
+    void onTagClicked(int tagId);
+};
+
+#endif // CATEGORIESANDTAGSWIDGET_H
