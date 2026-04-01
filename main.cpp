865ec7273eb2a70b738e6eea9043fbfe65c40fbd
Artem <sserz.h@bk.ru>
2026-04-01 11:09:13 +0300

    main.cpp: инициализация бд до создания главного виджета

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.h"
-
+#include "database.h"
 #include <QApplication>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    if (!Database::instance().connectDatabase()) {
+        qDebug() << "Не удалось подключиться к БД.";
+        return -1;
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
